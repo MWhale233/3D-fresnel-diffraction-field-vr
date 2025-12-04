427d316f8db58abf336881f1d7d64fecbcb4f871
Pucong Li <140274533+MWhale233@users.noreply.github.com>
2025-12-04 23:54:52 +0100

    Update the debug breakpoints
    The project is stopped temporarily, feel warm to upload again

--- fresnel-vr/Assets/Scripts/Fresnel diffraction/05.03.2025/FresnelGenerator.cs
+++ fresnel-vr/Assets/Scripts/Fresnel diffraction/05.03.2025/FresnelGenerator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2bf22302bc4dd84d346bb203552cdf5280a254b9f3cb44db89f094f9cd6ea94f
-size 9641
+oid sha256:60e908368a1b00e63e1eed9cedeeab3510012f2cd23d21537c68b8cad8db0825
+size 9679
